4799bd539c3a224ca9f4e4acb0a9aa95874f7670
Dariusz Stawik <dariusz.stawik@gmail.com>
2023-11-21 16:11:11 +0100

    Add about page

---
+++ src/app/[locale]/components/global-components/paragraph-with-image-on-the-right/index.js
@@ -0,0 +1,52 @@
+"use client";
+
+import { Slide } from "react-awesome-reveal";
+import SectionTitle from "../section-title";
+import Button from "../button";
+import Link from "next/link";
+
+export default function ParagraphWithImageOnTheRight({
+  title,
+  children,
+  img,
+  buttonTitle,
+  buttonLink,
+}) {
+  return (
+    <div className={`w-full py-16 flex flex-col lg:flex-row-reverse`}>
+      <Slide
+        direction="left"
+        delay={600}
+        triggerOnce
+        className="w-1/2 h-full flex justify-center items-start"
+      >
+        <div
+          className=""
+          style={{
+            clipPath:
+              "polygon(66% 0, 88% 21%, 100% 65%, 81% 100%, 20% 100%, 0 64%, 22% 14% )",
+            width: "600px",
+            height: "440px",
+          }}
+        >
+          <img
+            src={img?.fields.file.url ? img.fields.file.url : ""}
+            alt={
+              img?.fields.file.description ? img.fields.file.description : ""
+            }
+            className="rounded-md"
+          />
+        </div>
+      </Slide>
+      <div className="w-1/2 ml-16 mr-16 lg:ml-16 lg:mr-32 flex flex-col gap-2 text-lg">
+        <SectionTitle isAlignedLeft>{title}</SectionTitle>
+        {children}
+        {buttonTitle && (
+          <Button className="mr-auto mt-6">
+            <Link href="/">{buttonTitle}</Link>
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ src/app/[locale]/o-nas/page.js
@@ -0,0 +1,11 @@
+import ParagraphWithImageOnTheLeft from "../components/global-components/paragraph-with-image-on-the-left";
+import ParagraphWithImageOnTheRight from "../components/global-components/paragraph-with-image-on-the-right";
+
+export default function oNas() {
+  return (
+    <div>
+      <ParagraphWithImageOnTheLeft></ParagraphWithImageOnTheLeft>
+      <ParagraphWithImageOnTheRight></ParagraphWithImageOnTheRight>
+    </div>
+  );
+}
