c29d8338e17c3f8aa3aa9d2c3546c90cfa24214a
Dariusz Stawik <dariusz.stawik@gmail.com>
2023-11-28 21:32:06 +0100

    Update pahe header styling

--- src/app/[locale]/components/global-components/page-header/index.js
+++ src/app/[locale]/components/global-components/page-header/index.js
@@ -7,19 +7,14 @@ const PageHeader = ({ children }) => {
         backgroundPosition: "top",
       }}
     >
-      <h1 className="text-4xl lg:text-5xl text-white z-20">{children}</h1>
+      <h1
+        className="tracking-[.03em] leading-relaxed text-4xl lg:text-5xl text-white z-20"
+        style={{ textShadow: "1px 2px 4px rgba(0, 0, 0, 0.8)" }}
+      >
+        {children}
+      </h1>
     </div>
   );
 };
 
 export default PageHeader;
-
-{
-  /* <div
-      className="mt-28 relative w-screen px-32 py-40 bg-primary flex after:content-[''] after:absolute after:top-0 after:left-0 after:w-full after:h-full after:bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-transparent to-darkGray"
-      style={{
-        backgroundImage: 'url("/group22.jpg")',
-        backgroundPosition: "top",
-      }}
-    ></div> */
-}
